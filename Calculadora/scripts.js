24d7899e76f10d6a4bdd6acf98e3c1490ea961a7
marlokzy <marlon.r12325@gmail.com>
2023-08-11 19:45:38 -0300

    add code

--- Calculadora/scripts.js
+++ Calculadora/scripts.js
@@ -1,3 +1,30 @@
-console.log("Olá, mundo!");
+// Seleção dos elementos
+const display = document.querySelector("#displayInput");
+const botaoIgual = document.querySelector(".igual");
+const botaoPonto = document.querySelector(".ponto");
+const botoesNum = document.querySelectorAll(".num");
+const botoesOpe = document.querySelector(".operadores");
 
-// continuar em 2:59:55
+// Variáveis globais
+let operacaoAtual = "";
+let operador = null;
+let valorAnterior = "";
+let calculando = false;
+
+// Funções
+const atualizaDisplay = () => {
+  display.value = operacaoAtual;
+};
+
+const insere_numero = (evento) => {
+  if (calculando) {
+    operacaoAtual = evento.target.textContent;
+    calculando == false;
+  } else {
+    operacaoAtual += evento.target.textContent;
+  }
+  atualizaDisplay();
+};
+
+// Eventos
+botoesNum.forEach((botao) => botao.addEventListener("click", insere_numero));
